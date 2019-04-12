d41bdfee8286ff2617c06bb2f6e5a61b0c0eaac9
Chris Mayers <cmayers@csumb.edu>
2019-04-12 02:18:50 +0000

    adding lab 7 files

--- labs/lab7/api/getProducts.php
+++ labs/lab7/api/getProducts.php
@@ -3,7 +3,7 @@
 include "dbConnection.php";
 $conn = getDatabaseConnection("ottermart");
 
-$sql = "SELECT * FROM om_product ORDER BY price";
+$sql = "SELECT * FROM om_product ORDER BY productPrice";
 $stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
